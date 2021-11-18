5e20202ce367ebe5123d480ae328c6a8e652c013
ozwierz <78513215+ozwierz@users.noreply.github.com>
2021-11-18 12:12:51 -0500

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,76 @@
+var video = document.querySelector("#player1");
+var volume = document.querySelector("#volume");
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+});
+
+// PLAY 
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play()
+	let vol = volume.innerHTMl = (video.volume * 100) + "%"
+});
+
+// PAUSE
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause()
+});
+
+// SLOWER
+document.querySelector("#slower").addEventListener("click", function() {
+	video.playbackRate = video.playbackRate * 0.95;
+	console.log("New speed is " + video.playbackRate)
+});
+
+// FASTER
+document.querySelector("#faster").addEventListener("click", function() {
+	video.playbackRate = video.playbackRate * 1.05;
+	console.log("New speed is " + video.playbackRate)
+});
+
+// SKIP 
+document.querySelector("#skip").addEventListener("click", function() {
+	if (video.currentTime < video.duration - 15) {
+		video.currentTime += 15;
+	}
+	else {
+		video.currentTime = 0;
+		console.log("Beginning")
+	}
+	console.log( "Current location is " + video.currentTime)
+	video.play()
+});
+
+// MUTE
+document.querySelector("#mute").addEventListener("click", function() {
+	console.log("Mute Video")
+	if (video.muted ===false) {
+		video.muted = true;
+		this.innerHTML = "Unmute"
+	}
+	else {
+		this.innerHTML = "Mute"
+		video.muted = false;
+	}
+});
+
+// VOLUME SLIDER
+document.querySelector("#slider").addEventListener("change", function() {
+	let vol = volume.innerHTML = this.value + "%";
+	video.volume = (this.value/100);
+	console.log(this.value);
+});
+
+// STYLED 
+document.querySelector("#vintage").addEventListener("click", function() {
+	console.log('Vintage');
+	video.classList.add("oldSchool");
+});
+
+// ORIGINAL 
+document.querySelector("#orig").addEventListener("click", function() {
+	console.log('Original');
+	video.classList.remove("oldSchool");
+});
